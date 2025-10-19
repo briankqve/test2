695f22fb08078a87eb246633817fed010fa7923a
Brian <b9240322@gmail.com>
2025-10-19 07:52:56 +0300

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
         <meta charSet="utf-8" />
         <meta name="theme-color" content="#000000" />
-        <link rel="canonical" href="https://your-domain.vercel.app" />
+        <link rel="canonical" href="https://testik1-hzxt.vercel.app/" />
       </Head>
       <body>
         <Main />
@@ -15,3 +15,4 @@ export default function Document() {
     </Html>
   )
 }
+
